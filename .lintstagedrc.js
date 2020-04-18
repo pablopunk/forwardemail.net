9b98cfa55d4aced92791f7c78841050dc10ad467
niftylettuce <niftylettuce@gmail.com>
2020-04-18 17:03:56 -0500

    fix: conditionally remove dkim-signatures only if necessary

--- .lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "*.md,!test/snapshots/**/*.md,!test/**/snapshots/**/*.md,!locales/README.md": [
-    filenames => filenames.map(filename => `remark ${filename} -qfo`)
-  ],
-  'package.json': 'fixpack',
-  '*.js': 'xo --fix'
-};
